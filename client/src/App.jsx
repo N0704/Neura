cba942394fdd788de139105ae0de62562206a5eb
Nghia Bui <nghiabui1097@gmail.com>
2025-10-31 20:54:40 +0700

    feat: tạo giao diện đăng ký, đăng nhập

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router";
 import AuthLayout from "./layouts/AuthLayout";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
+import Home from "./pages/Home";
 
 function App() {
   return (
